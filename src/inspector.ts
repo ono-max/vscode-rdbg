ccb524de585ad7ea565ce5a7b7602adeaafb6445
ono-max <onoto1998@gmail.com>
2022-10-04 00:24:49 +0900

    fix

--- src/inspector.ts
+++ src/inspector.ts
@@ -286,13 +286,6 @@ class RdbgInspectorPanel {
 		}
 	}
 
-	private getRandColor() {
-		const r = Math.floor(Math.random() * 256)
-		const g = Math.floor(Math.random() * 256)
-		const b = Math.floor(Math.random() * 256)
-		return `rgba(${r}, ${g}, ${b}, 0.5)`
-	}
-
 	private updateWebview(records: any[], logIndex: number) {
 		if (records.length === 0) {
 			return
